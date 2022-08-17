999bad7e563d4b5e2924163c91880284f7a4dce3
brightonchege <brightonspak@gmail.com>
2022-08-17 20:53:16 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+const dodger = document.getElementById('dodger')
+function moveDodgerLeft(){
+  const moveit = dodger.style.left.replace("px","")
+  const left = parseInt(moveit,10)
+
+  if(left > 0){
+    dodger.style.left=`${left - 5}px`
+  }
+}
+function moveDodgerRight(){
+  const moveit = dodger.style.left.replace("px","")
+  const left = parseInt(moveit,10)
+
+  if(left > 0 && left < 360){
+    dodger.style.left=`${left + 5}px`
+  }
+}
+document.addEventListener('keydown',function(f){
+
+  if(f.key === "ArrowLeft"){
+    moveDodgerLeft()
+
+  }else if(f.key === "ArrowRight"){
+    moveDodgerRight()
+
+  }
+})
